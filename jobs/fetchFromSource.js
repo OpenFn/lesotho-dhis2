3f726e48a10661a6a4c8d09a995bc6ad067233c8
Taylor Downs <taylor@openfn.org>
2022-01-20 12:16:42 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/fetchFromSource.js
+++ jobs/fetchFromSource.js
@@ -1,9 +1,16 @@
 get('dataValueSets', {
-  dataSet: 'ETYQ9xrOgCI',
-  orgUnit: 'hfal93WttYV',
-  period: '202111',
-  children: 'true',
-});
+  dataSet: 'pBOMPrpg1QX',
+  orgUnit: 'DiszpKrYNg8',
+  period: '201401',
+  fields: '*',
+})
+
+// get('dataValueSets', {
+//   dataSet: 'ETYQ9xrOgCI',
+//   orgUnit: 'hfal93WttYV',
+//   period: '202111',
+//   children: 'true',
+// });
 
 fn(state => {
   console.log('For testing only... view the output');
