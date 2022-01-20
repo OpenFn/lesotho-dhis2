bc528d3872f1cf68e60613cd4a96ac5d9ae25e66
Taylor Downs <taylor@openfn.org>
2022-01-20 12:54:49 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/fetchFromSource.js
+++ jobs/fetchFromSource.js
@@ -1,9 +1,43 @@
-get('dataValueSets', {
-  dataSet: 'bkBzJ3ETIBD',
-  orgUnit: 'hfal93WttYV',
-  period: '202111',
-  fields: '*',
+fn(state => {
+  const orgunits = [
+    "hfal93WttYV",
+    "JEhqFsfXxTt",
+    "HqJwxVQhfyM",
+    "VSxknPhjR6o",
+    "M7qFOnwmE3A",
+    "Qq4jYe5tHnl",
+    "mNzjvxYEHkq",
+    "nFPxXeZftGm",
+    "eWjt9Zl76FS",
+    "uqh2OI3no6W",
+    "aeBwvrjdh7m",
+    "qnw9ul9mgww",
+    "Kts15CHhP3h",
+    "Er2eXRYQ5kD",
+    "mdpCE7IYau0",
+    "HR8JDs4Sae5",
+    "GBeQB9YNmP4",
+    "L4FwAUd37Wp",
+    "KlCB0HQHtbg",
+    "dJssgIzIiL4",
+    "TBh00t5LnBZ",
+    "lpjb08mkXcY",
+    "soweCPFSM7L",
+    "isI5LRdu80m",
+  ]
+  return { ...state, orgunits };
 })
+
+
+
+each("$.orgunits[*]",
+  get('dataValueSets', {
+    dataSet: 'bkBzJ3ETIBD',
+    orgUnit: state.data,
+    period: '202111',
+    fields: '*',
+  })
+);
 
 // get('dataValueSets', {
 //   dataSet: 'ETYQ9xrOgCI',
