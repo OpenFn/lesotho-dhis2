8e1bf0ab171662e0c7bf38a048f23a16d910cb26
Taylor Downs <taylor@openfn.org>
2022-01-24 05:39:25 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/fetchFromSource.js
+++ jobs/fetchFromSource.js
@@ -1,4 +1,31 @@
 fn(state => {
+  const orgunits = [
+    "hfal93WttYV",
+    "JEhqFsfXxTt",
+    "HqJwxVQhfyM",
+    // "VSxknPhjR6o",
+    // "M7qFOnwmE3A",
+    // "Qq4jYe5tHnl",
+    // "mNzjvxYEHkq",
+    // "nFPxXeZftGm",
+    // "eWjt9Zl76FS",
+    // "uqh2OI3no6W",
+    // "aeBwvrjdh7m",
+    // "qnw9ul9mgww",
+    // "Kts15CHhP3h",
+    // "Er2eXRYQ5kD",
+    // "mdpCE7IYau0",
+    // "HR8JDs4Sae5",
+    // "GBeQB9YNmP4",
+    // "L4FwAUd37Wp",
+    // "KlCB0HQHtbg",
+    // "dJssgIzIiL4",
+    // "TBh00t5LnBZ",
+    // "lpjb08mkXcY",
+    // "soweCPFSM7L",
+    // "isI5LRdu80m",
+  ]
+  
   const categoriesMapping = {
     ETYQ9xrOgCI: 'PITC',
     tsVPADeBpHd: 'CITC',
@@ -41,37 +68,8 @@ fn(state => {
     }
     return ''
   }
-  return { ...state, getCategory, getDissegration };
-})
-
-fn(state => {
-  const orgunits = [
-    "hfal93WttYV",
-    "JEhqFsfXxTt",
-    "HqJwxVQhfyM",
-    // "VSxknPhjR6o",
-    // "M7qFOnwmE3A",
-    // "Qq4jYe5tHnl",
-    // "mNzjvxYEHkq",
-    // "nFPxXeZftGm",
-    // "eWjt9Zl76FS",
-    // "uqh2OI3no6W",
-    // "aeBwvrjdh7m",
-    // "qnw9ul9mgww",
-    // "Kts15CHhP3h",
-    // "Er2eXRYQ5kD",
-    // "mdpCE7IYau0",
-    // "HR8JDs4Sae5",
-    // "GBeQB9YNmP4",
-    // "L4FwAUd37Wp",
-    // "KlCB0HQHtbg",
-    // "dJssgIzIiL4",
-    // "TBh00t5LnBZ",
-    // "lpjb08mkXcY",
-    // "soweCPFSM7L",
-    // "isI5LRdu80m",
-  ]
-  return { ...state, dataSet: 'bkBzJ3ETIBD', fields: '*', period: '202111', orgunits, dataValues: []};
+  
+  return { ...state, dataSet: 'bkBzJ3ETIBD', fields: '*', period: '202111', orgunits, dataValues: [], getCategory, getDissegration};
 })
 
 each("orgunits[*]", get('dataValueSets', {
