91b2f6c93795d8c61d4ec88f82a904b69f92e50f
Taylor Downs <taylor@openfn.org>
2022-01-24 05:16:55 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/fetchFromSource.js
+++ jobs/fetchFromSource.js
@@ -44,7 +44,13 @@ each("orgunits[*]", get('dataValueSets', {
 
 fn(state => {
   const getCategory = dataElement => {
-    return dataElement == 'ETYQ9xrOgCI' ? 'PITC' : dataElement == 'tsVPADeBpHd' ? 'CITC' : dataElement == 'BMiVQoY0NzQ' ? 'Self-Test' : ''
+    return dataElement == 'ETYQ9xrOgCI' ? '' : dataElement == '' ? '' : dataElement == '' ? 'Self-Test' : ''
+  }
+  
+  const categoriesMapping = {
+    ETYQ9xrOgCI: 'PITC',
+    tsVPADeBpHd: 'CITC',
+    BMiVQoY0NzQ: 'Self-Test'
   }
   
   const dissegregationsMapping = {
