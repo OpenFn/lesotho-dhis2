bbad649e8cae854ee9dc2a3230f4ff329c2ccf26
Taylor Downs <taylor@openfn.org>
2022-01-24 04:36:59 +0000

    elias@openfn.org's change on OpenFn.org

--- jobs/fetchFromSource.js
+++ jobs/fetchFromSource.js
@@ -1,29 +1,29 @@
 fn(state => {
   const orgunits = [
     "hfal93WttYV",
-    "JEhqFsfXxTt",
-    "HqJwxVQhfyM",
-    "VSxknPhjR6o",
-    "M7qFOnwmE3A",
-    "Qq4jYe5tHnl",
-    "mNzjvxYEHkq",
-    "nFPxXeZftGm",
-    "eWjt9Zl76FS",
-    "uqh2OI3no6W",
-    "aeBwvrjdh7m",
-    "qnw9ul9mgww",
-    "Kts15CHhP3h",
-    "Er2eXRYQ5kD",
-    "mdpCE7IYau0",
-    "HR8JDs4Sae5",
-    "GBeQB9YNmP4",
-    "L4FwAUd37Wp",
-    "KlCB0HQHtbg",
-    "dJssgIzIiL4",
-    "TBh00t5LnBZ",
-    "lpjb08mkXcY",
-    "soweCPFSM7L",
-    "isI5LRdu80m",
+    // "JEhqFsfXxTt",
+    // "HqJwxVQhfyM",
+    // "VSxknPhjR6o",
+    // "M7qFOnwmE3A",
+    // "Qq4jYe5tHnl",
+    // "mNzjvxYEHkq",
+    // "nFPxXeZftGm",
+    // "eWjt9Zl76FS",
+    // "uqh2OI3no6W",
+    // "aeBwvrjdh7m",
+    // "qnw9ul9mgww",
+    // "Kts15CHhP3h",
+    // "Er2eXRYQ5kD",
+    // "mdpCE7IYau0",
+    // "HR8JDs4Sae5",
+    // "GBeQB9YNmP4",
+    // "L4FwAUd37Wp",
+    // "KlCB0HQHtbg",
+    // "dJssgIzIiL4",
+    // "TBh00t5LnBZ",
+    // "lpjb08mkXcY",
+    // "soweCPFSM7L",
+    // "isI5LRdu80m",
   ]
   return { ...state, dataSet: 'bkBzJ3ETIBD', fields: '*', period: '202111', orgunits, dataValues: []};
 })
@@ -35,29 +35,33 @@ each("orgunits[*]", get('dataValueSets', {
   fields: state => state.fields,
   children: true
 }, {}, state => {
-  
+  console.log('--------------------------------------------------------------------------------')
   console.log(JSON.stringify(state.dataValues, null, 2))
   console.log(JSON.stringify(state.data.dataValues, null, 2))
   return { ...state, dataValues: [ ...state.dataValues, ...state.data.dataValues]}
+  console.log('--------------------------------------------------------------------------------')
 }));
 
-// fn(state => {
-//   console.log('For testing only... view the output');
-//   console.log(JSON.stringify(state.dataValues, null, 2));
-//   return state;
-// });
+fn(state => {
+  const testCategoryMapping = {
+    ETYQ9xrOgCI: 'PITC',
+    tsVPADeBpHd: 'CITC',
+    BMiVQoY0NzQ: 'SELFTEST'
+  }
+  return { ...state, testCategoryMapping };
+})
 
 fn(state => {
-  
   let categories = {
     PITC: [],
     CITC: [],
     SELFTEST: []
   }
   
-  // for (let dataValue of state.dataValues) {
-  //   console.log(dataValue)
-  // }
+  for (let dataValue of state.dataValues) {
+    categories[state.testCategoryMapping[dataValue.dataElement]].push(dataValue)
+  }
   
+  console.log(JSON.stringify(categories, null, 2))
   return state;
 })
