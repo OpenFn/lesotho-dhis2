a181ea01ad50908a4c96338dfed3c283f757db54
Elias W. BA <elias@openfn.org>
2022-01-25 17:56:40 +0000

    Load data elements to destination system

--- jobs/loadToDestination.js
+++ jobs/loadToDestination.js
@@ -1,9 +1,4 @@
-fn(state => {
-  console.log(
-    'For testing only, see how the final state of the previous',
-    'job becomes the initial state for this job:',
-    state.data
-  );
-});
-
-create('dataValueSets', state.data);
+each(
+  dataPath('dataElements[*]'),
+  create('dataValueSets', state => state.data)
+);
