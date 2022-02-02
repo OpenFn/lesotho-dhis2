15841fa55ab5425d4434e630f3d3929fb73f6da6
Taylor Downs <taylor@openfn.org>
2022-02-02 18:13:46 +0000

    taylor@openfn.org's change on OpenFn.org

--- jobs/loadToDestination.js
+++ jobs/loadToDestination.js
@@ -1,6 +1,6 @@
 fn(state => {
   console.log("We've done the data manipulation in the previous step.");
-  console.log(state.data);
+  // console.log(state.data);
   console.log('Here, we load into the new system with the new credential.');
   return state;
 });
